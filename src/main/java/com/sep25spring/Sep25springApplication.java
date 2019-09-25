f9a81385e6f55678f46200807aebe0084bec7055
Matt Cadorette <mcadorette@gmail.com>
2019-09-25 14:34:03 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/sep25spring/Sep25springApplication.java
@@ -0,0 +1,12 @@
+package com.sep25spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep25springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sep25springApplication.class, args);
+	}
+}
